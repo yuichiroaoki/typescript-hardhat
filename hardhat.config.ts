6e7abe5111d9a6b10ef21789de2a53fafb061224
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-24 12:08:50 +0900

    solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,14 +10,20 @@ import "./tasks/accounts";
 import "./tasks/balance";
 import "./tasks/block-number";
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.6.12",
+      },
+    ],
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
