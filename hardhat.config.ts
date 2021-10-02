4a01707a971f2ca54151e36be63ef2488f0d671a
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-10-02 18:44:48 +0900

    hardhat-upgrades

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import "@openzeppelin/hardhat-upgrades";
 import "solidity-coverage";
 
 import "./tasks/accounts";
