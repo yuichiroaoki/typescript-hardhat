71dec8bfea070ac91e864c39daa8d1c2c439eac7
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-17 14:09:41 +0900

    update hardhat conf

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,22 +10,31 @@ import "solidity-coverage";
 import "./tasks/accounts";
 import "./tasks/balance";
 import "./tasks/block-number";
+import { HardhatUserConfig } from "hardhat/types";
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
         version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.6.12",
       },
     ],
   },
+  mocha: {
+    timeout: 200000,
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
+module.exports = config;
