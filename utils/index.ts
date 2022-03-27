bcfe079fac0d21ce265a992c597c32050b67f04e
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-03-27 11:28:43 +0900

    upgrade getBigNumber

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,8 @@
 import { ethers } from "hardhat";
 import { ERC20Mock__factory } from "../typechain";
-const { BigNumber } = ethers;
 
-const BASE_TEN = 10;
-
-// Defaults to e18 using amount * 10^18
 export const getBigNumber = (amount: number, decimals = 18) => {
-  return BigNumber.from(amount).mul(BigNumber.from(BASE_TEN).pow(decimals));
+  return ethers.utils.parseUnits(amount.toString(), decimals);
 };
 
 export async function advanceBlock() {
